7a1fd0c87a4ca940b0c73edece42826116830863
amrfahmy <amrfahmy57@gmail.com>
2019-09-08 16:55:01 +0200

    add introduction

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+
+  $(function() {
+  AOS.init();
+});
+
+$(window).on('load', function() {
+  AOS.refresh();
+});
+
   // Sidebar toggle behavior
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar, #content').toggleClass('active');
