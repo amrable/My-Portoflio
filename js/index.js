c7bcd65ecff5bfc36e57a96b13d505a1ad6c807d
amrfahmy <amrfahmy57@gmail.com>
2019-09-12 01:38:06 +0200

    fix css issue

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
+
+
 
   $(function() {
-  AOS.init({
-   easing: 'ease-out-back',
-   duration: 800,
-   delay: 300,
-   once: true,
-   disable: 'mobile'
- }
-);
-});
+    AOS.init({
+      easing: 'ease-out-back',
+      duration: 800,
+      delay: 300,
+      once: true,
+      disable: 'mobile'
+    });
+  });
 
-$(window).on('load', function() {
-  AOS.refresh();
-});
+  $(window).on('load', function() {
+    AOS.refresh();
+  });
 
   // Sidebar toggle behavior
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar, #content').toggleClass('active');
   });
 });
+
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+});
+
+console.log("alo");
